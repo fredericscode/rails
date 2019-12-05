5bbbb16e344e88699687a8a38a39d315d12cc28e
Frederic Mbea <fredericmbea@gmail.com>
2019-12-05 00:16:41 +0000

    Added hte scrolling effect put partials into a partials folder

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,25 @@
 module HomeHelper
     def links
       if user_signed_in?
-        'home/nav/dropdown'
+        'home/partials/nav/dropdown'
       else
-        'home/nav/auth_links'
+        'home/partials/nav/auth_links'
       end
+    end
+    
+    def developers
+        if user_signed_in?
+            'home/partials/developers'
+        else
+            'home/partials/empty'
+        end
+    end
+    
+    def btnStart
+        if user_signed_in?
+            "home/partials/startbtn/scrollbtn"
+        else
+            "home/partials/startbtn/authbtn"
+        end
     end
 end
