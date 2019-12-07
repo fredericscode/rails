1826d4a3910879ea96a72b7244fd5891c45dcaaf
Fredericscode <fredericscode@gmail.com>
2019-12-07 04:57:32 +0000

    Added modals and event listeners for camera icons

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -34,6 +34,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
   
   
+  // Add an event listener to call buttons
+    var cameraIcons = document.getElementsByClassName('camera-icon');
+    for (let item of cameraIcons) {
+      item.addEventListener('click', (event) => {
+        event.preventDefault();
+        var recipient_id = item.getAttribute("data-id");
+        var recipient_name = item.getAttribute("data-name");
+        console.log(recipient_id);
+        console.log(recipient_name);
+        var recipient_name_modal = document.getElementById('recipient_name_modal');
+        recipient_name_modal.innerHTML = recipient_name;
+        $('#sender-notif-modal').modal('show');
+
+        //cable.call(recipient_id, recipient_name);
+      })
+    }
+
+    // Call the answer method when the answer_btn is clicked.
+    const answerBtn = document.getElementById("answer-call");
+    answerBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      var session_id = document.getElementById("session_id").innerHTML;
+      var sender_id = document.getElementById('sender_id').innerHTML;
+      console.log(session_id);
+      console.log("answer btn clicked");
+      $('#receiver-notif-modal').modal('hide');
+      //cable.answer(session_id, sender_id);
+    });
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   
