4feabe5ff83731532b49f560c35c625c12591e0a
Fredericscode <fredericscode@gmail.com>
2020-01-06 02:25:34 +0000

    Removed level attribute and styled the avatar upload

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,14 +1,34 @@
 import roomSubscriber from '../channels/room_channel'
 
 
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
     
   /* Initialize tooltips */ 
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+  
+  /* Avatar upload functionality for user registration */
+  var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+  }
+   
+  $(".file-upload").on('change', function(){
+      readURL(this);
+  });
+    
+  $(".upload-button").on('click', function() {
+      $(".file-upload").click();
+  });
+  
 
 
   /* Scroll to the developers section when the start button is clicked */
